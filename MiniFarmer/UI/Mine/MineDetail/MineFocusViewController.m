2da5de6c646080477416c8429763e5cfdeb732ac
yahui.zhang <yahui.zhang@mtime.com>
2015-12-08 23:19:24 +0800

    关注的人 -> 专家 cell的搭建。

--- MiniFarmer/UI/Mine/MineDetail/MineFocusViewController.m
+++ MiniFarmer/UI/Mine/MineDetail/MineFocusViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "MineFocusViewController.h"
+#import "YHSegmentView.h"
 
-@interface MineFocusViewController ()
+@interface MineFocusViewController ()<YHSegmentViewDelegate>
+
+@property (nonatomic, strong) YHSegmentView *segmentView;
 
 @end
 
@@ -16,14 +19,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    
     [self.view setBackgroundColor:[UIColor colorWithHexString:@"eeeeee"]];
     [self setBarTitle:@"关注的人"];
+    
+    [self.view addSubview:self.segmentView];
+    [self setupSegmentItem];
+    [self.segmentView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view.mas_left);
+        make.top.equalTo(self.view.mas_top).offset(self.yDispaceToTop);
+        make.size.mas_equalTo(CGSizeMake(kScreenSizeWidth, 47));
+    }];
 
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+- (void)setupSegmentItem
+{
+    NSArray *titles = @[@"专家",@"农友"];
+    
+    for (NSString *title in titles) {
+        YHSegmentItem *item = [[YHSegmentItem alloc] init];
+        item.title = title;
+        item.font = [UIFont systemFontOfSize:16];
+        [self.segmentView addSegmentItem:item];
+    }
 }
+
+
+#pragma mark - YHSegmentViewDelegate
+- (void)segmentView:(YHSegmentView *)segmentView didSelectedAtIndex:(NSInteger)index
+{
+    
+}
+
+#pragma mark - init views
+-(YHSegmentView *)segmentView
+{
+    if (!_segmentView)
+    {
+        _segmentView = [[YHSegmentView alloc] init];
+        _segmentView.backgroundColor = [UIColor whiteColor];
+        _segmentView.itemToLeft = 64;
+        _segmentView.directionLineHeigth = 4;
+        _segmentView.itemsDispace = 110;
+        _segmentView.textSelectedColor = [UIColor colorWithHexString:@"#ff6633"];
+        _segmentView.textNormalColor = [UIColor colorWithHexString:@"#666666"];
+        _segmentView.directionLineColor = [UIColor colorWithHexString:@"#ff6633"];
+        _segmentView.bottomLineHeigth = 1;
+        _segmentView.bottomLineColor = [UIColor colorWithHexString:@"#e4e4e4"];
+        _segmentView.delegate = self;
+    }
+    return _segmentView;
+}
+
 
 
 
